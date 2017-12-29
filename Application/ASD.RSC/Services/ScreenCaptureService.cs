17c4706769b2a82d13462f954a7f56b4e831b2a8
Stanislav Kuzmich [Art.Stea1th] <art.stea1th@gmail.com>
2017-12-29 04:11:28 +0300

    quick impl.

---
+++ Application/ASD.RSC/Services/ScreenCaptureService.cs
@@ -0,0 +1,74 @@
+﻿using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+using D = System.Drawing;
+
+namespace ASD.RSC.Services {
+
+    internal sealed class ScreenCaptureService {
+
+        // System.Drawing
+
+        private D.Imaging.PixelFormat dFormat = D.Imaging.PixelFormat.Format32bppArgb;
+        private D.Imaging.ImageLockMode dLockMode = D.Imaging.ImageLockMode.ReadOnly;
+        private D.Rectangle dRect;
+
+        // System.Windows.Media
+
+        private PixelFormat format = PixelFormats.Bgra32;
+        private int bufferSize;
+        private Int32Rect rect;
+
+        public WriteableBitmap ScreenBuffer { get; private set; }
+
+        // Common
+
+        private int width, height;
+
+        public ScreenCaptureService() => ValidateScreenParameters();
+
+        public void UpdateScreen() {
+
+            using (var bitmap = new D.Bitmap(width, height, dFormat)) {
+                using (var graphics = D.Graphics.FromImage(bitmap)) {
+
+                    ValidateScreenParameters();
+                    graphics.CopyFromScreen(0, 0, 0, 0, bitmap.Size);
+
+                    var dataPtr = bitmap.LockBits(dRect, dLockMode, dFormat);
+                    ScreenBuffer.WritePixels(rect, dataPtr.Scan0, bufferSize, ScreenBuffer.BackBufferStride);
+                    bitmap.UnlockBits(dataPtr);
+                }
+            }
+        }
+
+        private void ValidateScreenParameters() {
+            if (ScreenSizeUpdated()) {
+                UpdateRectangles();
+                ReallocateScreenBuffer();
+            }
+        }
+
+        private bool ScreenSizeUpdated() {
+
+            var newWidth = (int)SystemParameters.PrimaryScreenWidth;
+            var newHeight = (int)SystemParameters.PrimaryScreenHeight;
+
+            if (width == newWidth && height == newHeight) {
+                return false;
+            }
+            width = newWidth; height = newHeight; return true;
+        }
+
+        private void UpdateRectangles() {
+            dRect = new D.Rectangle(0, 0, width, height);
+            rect = new Int32Rect(0, 0, width, height);
+        }
+
+        private void ReallocateScreenBuffer() {
+            ScreenBuffer = new WriteableBitmap(width, height, 96, 96, format, null);
+            bufferSize = ScreenBuffer.PixelHeight * ScreenBuffer.BackBufferStride;
+        }
+    }
+}
--- Application/ASD.RSC/ViewModels/ShellViewModel.cs
+++ Application/ASD.RSC/ViewModels/ShellViewModel.cs
@@ -1,8 +1,72 @@
-﻿namespace ASD.RSC.ViewModels {
+﻿using System;
+using System.Reflection;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Threading;
+
+namespace ASD.RSC.ViewModels {
 
     using Helpers;
+    using Services;
 
-    public sealed class ShellViewModel : Observable {
+    internal enum State { Stopped, Started }
 
+    internal sealed class ShellViewModel : Observable {
+
+        private readonly string title = Assembly.GetExecutingAssembly().GetCustomAttribute<AssemblyTitleAttribute>().Title;
+
+        private ScreenCaptureService screenCaptureService;
+
+        private RelayCommand startCommand, pauseCommand;
+        private RelayCommand startPauseCommand;
+        private State state;
+
+        private DispatcherTimer timer;
+
+        public string Title => $"{title} : {state}";
+        public ImageSource Screen => screenCaptureService.ScreenBuffer;
+
+        public ICommand StartPause {
+            get => startPauseCommand;
+            set => Set(ref startPauseCommand, (RelayCommand)value);
+        }
+
+        public ShellViewModel() {
+            InitializeScreenCapture();
+            InitializeCommands();
+        }
+
+        private void InitializeScreenCapture() {
+            screenCaptureService = new ScreenCaptureService();
+            timer = new DispatcherTimer();
+            timer.Interval = TimeSpan.FromTicks(1);
+            timer.Tick += (s, e) => screenCaptureService.UpdateScreen();
+        }
+
+        private void InitializeCommands() {
+            startCommand = new RelayCommand(
+                () => ChangeState(StartCapture, pauseCommand),
+                () => state == State.Stopped, "START");
+            pauseCommand = new RelayCommand(
+                () => ChangeState(PauseCapture, startCommand),
+                () => state == State.Started, "PAUSE");
+            ChangeState(PauseCapture, startCommand);
+        }
+
+        private void StartCapture() {
+            timer.Start();
+            state = State.Started;
+        }
+
+        private void PauseCapture() {
+            timer.Stop();
+            state = State.Stopped;
+        }
+
+        private void ChangeState(Action action, RelayCommand newStartPauseCommand) {
+            action?.Invoke();
+            StartPause = newStartPauseCommand;
+            OnPropertyChanged(nameof(Title));
+        }
     }
 }
